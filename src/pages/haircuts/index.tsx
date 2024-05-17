6bae44bb5ad82dd41f2a1f6be09261e34c231e06
joaovitorcruzz <joaovitordasilvacruz123@gmail.com>
2024-05-17 17:23:31 -0300

    NewHaircutAtt

--- src/pages/haircuts/index.tsx
+++ src/pages/haircuts/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import { Sidebar } from "@/src/components/sidebar";
 import {
@@ -15,10 +16,27 @@ import {
 import Link from "next/link";
 
 import { IoMdPricetag } from 'react-icons/io'
+import { canSSRAuth } from "@/src/utils/canSSRAuth";
+import { setupAPIClient } from "@/src/services/api";
 
-export default function Haircuts() {
+
+interface HaircutsItem {
+    id: string;
+    name: string;
+    price: number | string;
+    status: boolean;
+    user_id: string;
+}
+
+interface HaircutsProps {
+    haircuts: HaircutsItem[];
+}
+
+export default function Haircuts({ haircuts }: HaircutsProps) {
 
     const [isMobile] = useMediaQuery("(max-width: 500px)")
+
+    const [haircutList, setHaircutList] = useState<HaircutsItem[]>(haircuts || [])
 
     return (
         <>
@@ -60,31 +78,33 @@ export default function Haircuts() {
 
                     </Flex>
                     <div style={{ width: '100%' }}>
-                        <Link href="/haircuts/123">
-                            <Flex
-                                cursor="pointer"
-                                p={4}
-                                background="barber.400"
-                                direction={isMobile ? "column" : 'row'}
-                                align={isMobile ? "flex-start" : "center"}
-                                rounded="4"
-                                mb={2}
-                                justifyContent="space-between"
-                            >
+                        {haircutList.map(haircut => (
+                            <Link key={haircut.id} href={`/haircuts/${haircut.id}`}>
+                                <Flex
+                                    cursor="pointer"
+                                    p={4}
+                                    background="barber.400"
+                                    direction={isMobile ? "column" : 'row'}
+                                    align={isMobile ? "flex-start" : "center"}
+                                    rounded="4"
+                                    mb={2}
+                                    justifyContent="space-between"
+                                >
 
-                                <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
-                                    <IoMdPricetag size={28} color="#fba931" />
-                                    <Text color='white' fontWeight="bold" ml={4} noOfLines={2}>
-                                        Corte Completo
+                                    <Flex mb={isMobile ? 2 : 0} direction="row" alignItems="center" justifyContent="center">
+                                        <IoMdPricetag size={28} color="#fba931" />
+                                        <Text color='white' fontWeight="bold" ml={4} noOfLines={2}>
+                                           {haircut.name}
+                                        </Text>
+                                    </Flex>
+
+                                    <Text color='white' fontWeight="bold">
+                                        Preço: R$ {haircut.price}
                                     </Text>
+
                                 </Flex>
-
-                                <Text color='white' fontWeight="bold">
-                                    Preço: R$ 59.90
-                                </Text>
-
-                            </Flex>
-                        </Link>
+                            </Link>
+                        ))}
                     </div>
 
 
@@ -93,3 +113,43 @@ export default function Haircuts() {
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+    try {
+
+        const apiClient = setupAPIClient(ctx);
+        const response = await apiClient.get('/haircuts',
+            {
+                params: {
+                    status: true,
+                }
+            }
+        )
+
+        if (response.data === null) {
+            return {
+                redirect: {
+                    destination: '/dashboard',
+                    permanent: false,
+                }
+            }
+        }
+
+        return {
+            props: {
+                haircuts: response.data
+            }
+        }
+
+
+    } catch (err) {
+        console.log(err);
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false,
+            }
+        }
+    }
+})
--- src/pages/haircuts/new/index.tsx
+++ src/pages/haircuts/new/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Head from "next/head";
 import { Sidebar } from "@/src/components/sidebar";
 
@@ -11,11 +12,46 @@ import {
 } from '@chakra-ui/react'
 
 import { FiChevronLeft } from "react-icons/fi";
+import Router from "next/router";
 
 import Link from "next/link";
 
-export default function NewHaircut() {
+import { canSSRAuth } from "@/src/utils/canSSRAuth";
+import { setupAPIClient } from "@/src/services/api";
+
+interface NewHaircutProps {
+    subscription: boolean;
+    count: number;
+}
+
+export default function NewHaircut({ subscription, count }: NewHaircutProps) {
     const [isMobile] = useMediaQuery("(max-width: 500px)");
+
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+
+    async function handleRegister() {
+
+        if (name === '' || price === '') {
+            return;
+        }
+
+        try {
+            const apiClient = setupAPIClient();
+            await apiClient.post('/haircut', {
+                name: name,
+                price: Number(price),
+            })
+
+            Router.push('/haircuts')
+
+        } catch (err) {
+            console.log(err);
+            alert("Erro ao cadastrar esse modelo.")
+
+        }
+    }
+
     return (
         <>
             <Head>
@@ -32,7 +68,7 @@ export default function NewHaircut() {
                     >
                         <Link href="/haircuts">
                             <Button background="barber.400" color='white' p={4} display="flex" alignItems="center" justifyContent="center" mr={4}>
-                            <FiChevronLeft size={24} color="#FFF"/>
+                                <FiChevronLeft size={24} color="#FFF" />
                                 Voltar
                             </Button>
                         </Link>
@@ -45,27 +81,45 @@ export default function NewHaircut() {
                     <Flex maxW="700px" bg="barber.400" width="100%" alignItems="center" justifyContent="center" pt={8} pb={8} direction="column">
                         <Heading mb={4} color='white' fontSize={isMobile ? "22px" : "3xl"}>Cadastrar modelo</Heading>
                         <Input
-                        placeholder="Nome do corte"
-                        size="lg"
-                        type="text"
-                        w="85%"
-                        bg="gray.900"
-                        mb={4}
-                        textColor="white"
+                            placeholder="Nome do corte"
+                            size="lg"
+                            type="text"
+                            w="85%"
+                            bg="gray.900"
+                            mb={4}
+                            textColor="white"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
                         />
                         <Input
-                        placeholder="Valor do corte ex: 59.90"
-                        size="lg"
-                        type="text"
-                        w="85%"
-                        bg="gray.900"
-                        mb={4}
-                        textColor="white"
+                            placeholder="Valor do corte ex: 59.90"
+                            size="lg"
+                            type="text"
+                            w="85%"
+                            bg="gray.900"
+                            mb={4}
+                            textColor="white"
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}
                         />
 
-                        <Button w="85%" size="lg" color="gray.900" mb={6} bg="button.cta" _hover={{bg: "#FFb13e"}}>
+                        <Button onClick={handleRegister} w="85%" size="lg" color="gray.900" mb={6} bg="button.cta" _hover={{ bg: "#FFb13e" }} isDisabled={!subscription && count >= 3}>
                             Cadastrar
                         </Button>
+
+                        {!subscription && count >= 3 && (
+                            <Flex direction='row' alignItems="center" justifyContent="center">
+                                <Text textColor='white'>
+                                    Você atingiu seu limite de corte.
+                                </Text>
+                                <Link href='/planos'>
+                                    <Text fontWeight='bold' color="#31fb6a" cursor="pointer" ml={1}>
+                                        Seja premium
+                                    </Text>
+                                </Link>
+                            </Flex>
+                        )}
+
                     </Flex>
 
                 </Flex>
@@ -73,3 +127,28 @@ export default function NewHaircut() {
         </>
     )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    try {
+        const apiClient = setupAPIClient(ctx);
+
+        const response = await apiClient.get('/haircut/check')
+        const count = await apiClient.get('/haircut/count')
+
+        return {
+            props: {
+                subscription: response.data?.subscriptions?.status === 'active' ? true : false,
+                count: count.data
+            }
+        }
+
+    } catch (err) {
+        console.log(err);
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false,
+            }
+        }
+    }
+})
